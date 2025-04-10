465089e84b3ac4082ddef4b43b05a5b57e374d14
Kimminiah <patricialumbres18@gmail.com>
2025-04-11 00:17:24 +0800

    damn

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './navigation/AppNavigator';
+import { ThemeProvider } from './context/ThemeContext';
+import { requestPermissions } from './utils/permissions';
 
 export default function App() {
+  useEffect(() => {
+    requestPermissions();  
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
